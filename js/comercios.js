4a4f93e48c5df9d9e16a72c0d15d7f0d92c09773
Alfonso JL <ajavierlombardi@gmail.com>
2023-11-15 22:09:06 -0300

    vista de comercios en mapa

---
+++ js/comercios.js
@@ -0,0 +1,118 @@
+var comercios = [
+    {
+        name: "Comercio 1", 
+        direccion: "paunero 1231, San Miguel",
+        category: "electronica",
+        lat: "-34.541196116295296",
+        lng: "-58.71108035583838",        
+        hour: "Lun a Vier de 8 a 17hs",
+        telefone: "1538912389",
+        image:".image",
+        state:"habilitado"
+
+    },
+    {
+        name: "Comercio 2", 
+        direccion: "sarmiento 1155, San Miguel",
+        category: "jugueteria",
+        lat: "-34.544484379357435",
+        lng: "-58.70969268056797",        
+        hour: "Lun a Vier de 8 a 17hs",
+        telefone: "1538912389",
+        image:".image",
+        state:"habilitado"
+
+    },
+    {
+        name: "Comercio 3", 
+        direccion: "sarmiento 1305, San Miguel",
+        category: "ferreteria",
+        lat: "-34.54528487593846",
+        lng: "-58.708432119861385",        
+        hour: "Lun a Vier de 8 a 17hs",
+        telefone: "1538912389",
+        image:".image",
+        state:"habilitado"
+    },
+    {
+        name: "Comercio 4", 
+        direccion: "sarmiento 1155, San Miguel",
+        category: "farmacia",
+        lat: "-34.54140934215028",
+        lng: "-58.70864359194853",        
+        hour: "Lun a Vier de 8 a 17hs",
+        telefone: "1538912389",
+        image:".image",
+        state:"habilitado"
+    },
+    {
+        name: "Comercio 5", 
+        direccion: "balbin 1255, San Miguel",
+        category: "kiosco",
+        lat: "-34.53919544962383",
+        lng: "-58.71088199349825",        
+        hour: "Lun a Vier de 8 a 17hs",
+        telefone: "1538912389",
+        image:".image",
+        state:"habilitado"
+    },
+    {
+        name: "Comercio 6", 
+        direccion: "balbin 1255, San Miguel",
+        category: "artistica",
+        lat: "-34.539913381750665",
+        lng: "-58.71514610834764",        
+        hour: "Lun a Vier de 8 a 17hs",
+        telefone: "1538912389",
+        image:".image",
+        state:"habilitado"
+
+    },
+    {
+        name: "Comercio 7", 
+        direccion: "balbin 1255, San Miguel",
+        category: "gimnasio",
+        lat: "-34.54285414462936",
+        lng: "-58.71480278425375",        
+        hour: "Lun a Vier de 8 a 17hs",
+        telefone: "1538912389",
+        image:".image",
+        state:"habilitado"
+
+    },
+    {
+        name: "Comercio 8", 
+        direccion: "balbin 1255, San Miguel",
+        category: "hogar",
+        lat: "-34.54470997451545",
+        lng: "-58.71365949807422",        
+        hour: "Lun a Vier de 8 a 17hs",
+        telefone: "1538912389",
+        image:".image",
+        state:"habilitado"
+
+    },
+    {
+        name: "Comercio 9", 
+        direccion: "balbin 1255, San Miguel",
+        category: "agencia de viajes",
+        lat: "-34.54489384178039",
+        lng: "-58.7047038441862",        
+        hour: "Lun a Vier de 8 a 17hs",
+        telefone: "1538912389",
+        image:"./image",
+        state:"habilitado"
+
+    },
+    {
+        name: "Comercio 9", 
+        direccion: "balbin 1255, San Miguel",
+        category: "casa de deportes",
+        lat: "-34.546338665571746",
+        lng: "-58.70699684367054",        
+        hour: "Lun a Vier de 8 a 17hs",
+        telefone: "1538912389",
+        image:"./image",
+        state:"habilitado"
+    }
+]
